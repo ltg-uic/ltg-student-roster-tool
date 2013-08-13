20a70a737324c233090d0e2e2890e6af74f59a67
Alessandro Gnoli <tebemis@gmail.com>
2013-08-13 16:48:20 -0500

    Added Openfire XMPP support

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,6 +4,9 @@ import java.net.UnknownHostException;
 
 import org.codehaus.jackson.JsonNode;
 
+import play.libs.F.Promise;
+import play.libs.WS;
+import play.libs.WS.WSRequestHolder;
 import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -19,7 +22,7 @@ import com.mongodb.util.JSON;
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok("Welcome to LTG roster tool");
+		return ok(views.html.index.render());
 	}
 
 
@@ -44,6 +47,7 @@ public class Application extends Controller {
 		return ok(new BasicDBObject("run", run).append("roster", roster).toString()).as("application/json");
 	}
 
+	
 	@BodyParser.Of(BodyParser.Json.class)
 	public static Result addPersonToRun(String run) {
 		// Parse and validate
@@ -68,13 +72,50 @@ public class Application extends Controller {
 		} catch (MongoException e) {
 			return badRequest("A user with this name already exists, pick a different one");
 		}
-		// Create XMPP user
+		// Create XMPP user (if plugin is active)
+		WSRequestHolder req = WS.url("http://ltg.evl.uic.edu:9090/plugins/userService/userservice");
+		req.setQueryParameter("type", "add");
+		req.setQueryParameter("secret", "a6e58698e57a60acb3a8e20cbf3bdc3fdc9d8697");
+		req.setQueryParameter("username", json_body.getString("_id"));
+		req.setQueryParameter("password", json_body.getString("_id"));
+		Promise<WS.Response> res = req.get();
+		if (res.get().getBody().equals("<error>UserAlreadyExistsException</error>"))
+			return badRequest("A user with this name already exists, pick a different one");
+		if (res.get().getBody().equals("<error>RequestNotAuthorised</error>"))
+			return badRequest("Impossible to satisfy the request");
+		if (!res.get().getBody().equals("<result>ok</result>\n"))
+			return internalServerError("Myseterious error while trying to register the XMPP user");
 		return ok(json_body.toString()).as("application/json");
 	}
 
 
 	public static Result deletePersonInRun(String run, String person) {
+		// Delete from DB
+		try {
+			DBCollection people = new MongoClient().getDB("roster").getCollection("people");
+			people.remove(new BasicDBObject("_id", person));
+		} catch (Exception e) {
+			return internalServerError("Impossible to connect to DB");
+		}
+		// Delete XMPP user (if plugin is active)
+		WSRequestHolder req = WS.url("http://ltg.evl.uic.edu:9090/plugins/userService/userservice");
+		req.setQueryParameter("type", "delete");
+		req.setQueryParameter("secret", "a6e58698e57a60acb3a8e20cbf3bdc3fdc9d8697");
+		req.setQueryParameter("username", person);
+		Promise<WS.Response> res = req.get();
+		// TODO for future use...
+		//if (res.get().getBody().equals("<error>UserNotFoundException</error>"))
+		//return badRequest("A user with this name doesn't exist");
+		if (res.get().getBody().equals("<error>RequestNotAuthorised</error>"))
+			return badRequest("Impossible to satisfy the request");
+		if (!res.get().getBody().equals("<result>ok</result>\n"))
+			return internalServerError("Myseterious error while trying to register the XMPP user");
 		return ok();
+	}
+	
+	
+	public static Result updatePersonInRun(String run, String person) {
+		return notFound("Not yet implemented");
 	}
 
 }
